fb240fede3c5cc90c4a0671395f52a3e484f5ac1
Lenovo <erfanasheri2002@gmail.com>
2024-11-22 21:49:21 +0330

    refactoring

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 from PIL import Image
 import numpy as np
 
-def rgb_to_grayscale_manual(image_path):
+image_path = "myimage.jpg"
 
-    img = Image.open(image_path)
+img = Image.open(image_path)
 
-    pixels = np.array(img)
+pixels = np.array(img)
 
-    weights = [0.299, 0.587, 0.114]
+weights = [0.3, 0.6, 0.1]
 
-    gray_pixels = np.zeros(pixels.shape[:2], dtype=np.uint8)
+gray_pixels = np.zeros(pixels.shape[:2], dtype=np.uint8)
 
-    for i in range(pixels.shape[0]):
-        for j in range(pixels.shape[1]):
-            r, g, b = pixels[i, j]
-            gray_pixels[i, j] = int(weights[0]*r + weights[1]*g + weights[2]*b)
+for i in range(pixels.shape[0]):
+    for j in range(pixels.shape[1]):
+        r, g, b = pixels[i, j]
+        gray_pixels[i, j] = int(weights[0]*r + weights[1]*g + weights[2]*b)
 
-    gray_img = Image.fromarray(gray_pixels)
+gray_img = Image.fromarray(gray_pixels)
 
-    return gray_img
+gray_img.save("result.jpg")
 
-image_path = "MainAfter.jpg"
-
-gray_image = rgb_to_grayscale_manual(image_path)
-
-gray_image.save("gray_image_manual.jpg")
-
-gray_image.show()
+gray_img.show()
